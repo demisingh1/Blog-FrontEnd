dd17cce457d8d6daee09051ce32760b81cde2d7a
demisingh1 <jaswinder.cyrus1@gmail.com>
2023-12-12 09:21:12 +0530

    err removed

--- src/components/HomePostsList.js
+++ src/components/HomePostsList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const HomePostsList = ({...post}) => {
-    const{title,description, user,imgurl} = post
+    const{title, user,imgurl} = post
     console.log(post);
   return (
     <div className='post-card'>
       <div className='image-container'>
-      <img src={imgurl} alt="image name" className='img' />
+      <img src={imgurl} alt="splash name" className='img' />
       </div>
       <div className='card-text-container'>
       <h1>{title}</h1>        

0db153b9120773ae82f5cd011f7c2aad38d16f5a
demisingh1 <jaswinder.cyrus1@gmail.com>
2023-12-13 21:14:27 +0530

    a

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,7 +8,7 @@ import DashBoardPostList from '../components/DashBoardPostList'
 
 export const loader = async()=>{
     try {
-        const data = await axios.get('https://demiblogs.onrender.com/posts')
+        const data = await axios.get('https://demiblogs.onrender.com/Posts')
         console.log(data)
         const SingleUser = await axios.get('https://demiblogs.onrender.com/api/singleuser') // user detail fetched from database directly
         // console.log(SingleUser.data)
@@ -21,21 +21,21 @@ export const loader = async()=>{
 }
 
 const Dashboard = () => {
-    const {data} = useLoaderData()
+    // const {data} = useLoaderData()
 //   const {user } = useUserContext() // get USER DETAILS
 
-  const{dispatch , userPosts} = usePostContext();
+//   const{dispatch , userPosts} = usePostContext();
 
-   useEffect(()=>{
+//    useEffect(()=>{
     //  dispatch({type:USER_LOGIN, payload:SingleUser.data.message}) // no need to fetch the user AGAIn ALREADY FETCHED FROM THE LOGIN PAGE AND SAVED To LOCAL STORAGE
-     dispatch({type:USER_POSTS, payload:data.data.message})
-   },[data.data.message, dispatch])
+//      dispatch({type:USER_POSTS, payload:data.data.message})
+//    },[data.data.message, dispatch])
     
 
 return <div className='post-card-container'>
-    {userPosts.map((post)=>{
+    {/* {userPosts.map((post)=>{
         return <DashBoardPostList key={post._id} {...post} />
-    })}
+    })} */}
 </div>
 
 }
